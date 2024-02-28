77a2cd42def0c60feae50d0df6bae954c691b4ba
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2024-02-28 14:56:35 -0500

    added ChatGPT
    Can now compare the responses between Gemini and ChatGPT

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,40 @@
 import { useState } from "react";
 import styles from "./page.module.scss";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import Typewriter from "./typewriter";
+import OpenAI from "openai";
 
 export default function Home() {
+  const openai = new OpenAI({
+    apiKey: process.env.NEXT_PUBLIC_CHATGPT_API_KEY,
+    dangerouslyAllowBrowser: true,
+  });
+
   const [loading, setLoading] = useState(false);
   const [response, setResponse] = useState("");
+  const [response2, setResponse2] = useState("");
 
   function sendTest() {
     setLoading(true);
+    setResponse("");
+    setResponse2("");
     // Access your API key (see "Set up your API key" above)
     const genAI = new GoogleGenerativeAI(
       process.env.NEXT_PUBLIC_GOOGLE_GEMINI_API_KEY
     );
+
+    async function main(input) {
+      try {
+        const completion = await openai.chat.completions.create({
+          messages: [{ role: "user", content: input }],
+          model: "gpt-3.5-turbo",
+        });
+        console.log(completion);
+        setResponse2(completion.choices[0].message.content);
+      } catch (error) {
+        console.error(error);
+      }
+    }
 
     async function run(input) {
       try {
@@ -32,6 +55,7 @@ export default function Home() {
     }
 
     run(document.getElementById("input").value);
+    main(document.getElementById("input").value);
   }
 
   return (
@@ -43,7 +67,9 @@ export default function Home() {
           <form
             className={styles.contactForm}
             name="contact-form"
-            action={() => sendTest()}
+            action={() => {
+              sendTest();
+            }}
           >
             <label className="dark:text-white text-black" htmlFor="input">
               Input:
@@ -52,7 +78,20 @@ export default function Home() {
             <button type="submit">Submit</button>
           </form>
         )}
-        {response ? <p className={styles.response}>{response}</p> : null}
+        <div className={styles.responseContainer}>
+          {response ? (
+            <div className={styles.responseColumn}>
+              <h1>Gemini</h1>
+              <Typewriter text={response} delay={10} />
+            </div>
+          ) : null}
+          {response2 ? (
+            <div className={styles.responseColumn}>
+              <h1>ChatGPT</h1>
+              <Typewriter text={response2} delay={10} />
+            </div>
+          ) : null}
+        </div>
       </div>
     </main>
   );
